0ccacc88c34fa6302bfbf13ad2330e2816b8c2e2
nhwngoc03 <nhwngoc03@gmail.com>
2026-02-07 07:36:11 +0700

    fix deloy Git

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,24 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+          base: "/nong_san_xau_ma/",
+
       server: {
           base: "/FoodMarket-FE/", 
         port: 3000,
         host: '0.0.0.0',
       },
       plugins: [react({ babel: { plugins: [] } })],
+      build: {
+        chunkSizeWarningLimit: 1000,
+        rollupOptions: {
+          output: {
+            manualChunks: {
+              vendor: ['react', 'react-dom'],
+            }
+          }
+        }
+      },
       define: {
         'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
         'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY)
