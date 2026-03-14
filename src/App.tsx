53cc1c351e3f88b1a6f177760816f88325b860ad
Chaugiakiet <chaukiet2704@gmail.com>
2026-03-14 11:23:18 +0700

    fix scroll for system

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 import { AuthProvider } from './contexts/AuthContext';
 import AppRouter from './routes/AppRouter';
+import ScrollToTop from './components/layout/ScrollToTop';
 import './App.css';
 
 const App: React.FC = () => {
   return (
     <BrowserRouter basename="/nong_san_xau_ma/">
+      <ScrollToTop />
       <AuthProvider>
         <AppRouter />
       </AuthProvider>
