cd654c4fb7135c8298cbfa49128baf2fc4cadfef
Dev43 <vudinhsang0403@gmail.com>
2026-03-15 11:41:38 +0700

    Devloy

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
   const env = loadEnv(mode, '.', '');
   return {
-    base: "/nong_san_xau_ma/",
+    base: "/",
 
     server: {
       port: 3000,
