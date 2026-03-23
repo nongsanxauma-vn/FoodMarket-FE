e312edae34c6b77c6e905620c013c42cc3c87b20
Chaugiakiet <chaukiet2704@gmail.com>
2026-03-23 18:54:08 +0700

    fix merge 2 after merege refund

--- src/components/layout/Header.tsx
+++ src/components/layout/Header.tsx
@@ -341,11 +341,11 @@ const Header: React.FC<HeaderProps> = ({
                       <button onClick={() => { setShowProfileMenu(false); navigate('/my-returns'); }} className="w-full px-5 py-3 text-left text-xs font-bold hover:bg-gray-50 flex items-center gap-3 transition-colors">
                         <RefreshCw className="size-4 text-gray-400" /> Đổi trả / Hoàn tiền
                       </button>
-                      {(user?.role === AppRole.BUYER || user?.role === AppRole.FARMER) && (
+                      {/* {(user?.role === AppRole.BUYER || user?.role === AppRole.FARMER) && (
                         <button onClick={() => { setShowProfileMenu(false); navigate('/chat'); }} className="w-full px-5 py-3 text-left text-xs font-bold hover:bg-gray-50 flex items-center gap-3 transition-colors">
                           <MessageSquare className="size-4 text-gray-400" /> Tin nhắn
                         </button>
-                      )}
+                      )} */}
                       <div className="h-px bg-gray-50 mx-3 my-1" />
                       <button onClick={() => { setShowProfileMenu(false); onLogout(); }} className="w-full px-5 py-3 text-left text-xs font-black text-red-500 hover:bg-red-50 flex items-center gap-3 transition-colors">
                         <LogOut className="size-4" /> Đăng xuất
